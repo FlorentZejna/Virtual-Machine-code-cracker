5b6cfcc0830ec0db464506cc565c90d61fe980a7
Florent Zejna <florent.zejna@gmail.com>
2022-05-26 16:22:45 +0200

    implement reading a File in a readFile func

--- main.go
+++ main.go
@@ -1,23 +1,34 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/knownhosts"
+	"os"
 
 	"log"
 )
 
-var username string = "me"
-var password string = "1234"
-var host string = "192.168.100.145"
+func readFile(f string) (data []string, err error) {
+	b, err := os.Open(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer b.Close()
+	scanner := bufio.NewScanner(b)
+	for scanner.Scan() {
+		data = append(data, scanner.Text())
+	}
+	return data, err
+}
 
 func main() {
 	hostKeyCallback, err := knownhosts.New("/home/beriflapp/.ssh/known_hosts")
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	config := &ssh.ClientConfig{
 		User: username,
 		Auth: []ssh.AuthMethod{
@@ -32,5 +43,15 @@ func main() {
 	}
 	fmt.Println("access")
 	defer conn.Close()
+
+	passwords, err := readFile("/home/beriflapp/rootPass/words_alpha.txt")
+	if err != nil {
+		log.Println("Can't read file!")
+		os.Exit(1)
+	}
+
+	for _, pass := range passwords {
+		fmt.Println(pass)
+	}
 
 }
